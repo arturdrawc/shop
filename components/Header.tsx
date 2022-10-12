8438e7424f7b15bfbe01d71e24c66d143bdbbc94
Artur Drawc <artur.drawc@gmail.com>
2022-10-12 21:15:58 +0200

    Split app to components

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import router from "next/router";
+
+export const Header = () => {
+  return (
+    <header className="max-w-md mx-auto w-full">
+      <nav className="bg-gray-700 text-white px-4 py-2">
+        <Link href="/">
+          <a>Główna</a>
+        </Link>
+      
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </nav>
+    </header>
+  );
+}
