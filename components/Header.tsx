b3867292f35a23b7ace79f6161e788c93ef427e3
Artur Drawc <artur.drawc@gmail.com>
2022-10-12 22:12:18 +0200

    Change styles of Header & Footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,40 @@
 import Link from "next/link";
-import router from "next/router";
+import { useRouter } from "next/router";
+
+const routes = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+];
 
 export const Header = () => {
+  const router = useRouter();
+
   return (
-    <header className="max-w-md mx-auto w-full">
-      <nav className="bg-gray-700 text-white px-4 py-2">
-        <Link href="/">
-          <a>Główna</a>
-        </Link>
-      
-        <Link href="/about">
-          <a>About</a>
-        </Link>
+    <header>
+      <nav className="bg-gray-800 px-6 lg:px-8 flex h-16 items-center">
+        <div className="flex space-x-4">
+          {routes.map((route, index) => {
+            return (
+              <Link
+                key={index}
+                href={route.href}
+              >
+                <a className={
+                    "px-3 py-2 rounded-md text-sm font-medium "
+                    + (
+                        router.pathname === route.href
+                          ? "bg-gray-900 text-white"
+                          : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                        )
+                  }
+                >
+                  {route.name}
+                </a>
+              </Link>
+            );
+          })}
+        </div>
       </nav>
     </header>
   );
-}
+};
